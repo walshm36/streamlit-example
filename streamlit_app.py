95915b670e90b20952dd6f67d70ce97d3f9bb580
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-03-30 21:25:57 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,9 @@ with st.echo(code_location='below'):
     
 """
 st.title('My title')
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
