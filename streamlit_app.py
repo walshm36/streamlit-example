2ba50c6d8350617e2828d428223324ae5c134cdd
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 19:52:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import plotly as py
+! pip freeze   # optional: this is to check current installed package
+! pip install plotly --upgrade   # this to update plotly to latest version, at the time of this post: 4.5.4
+! pip install plotly   # might be unnecessary, but I did this
+import plotly.express as xp   # now it works
 
 
 st.title('Maeves Covid Dashboard')                    
