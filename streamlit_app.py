30e272f965f1d61bb582754fbf4276013ac000c0
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 19:34:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 
 
 st.title('Maeves Covid Dashboard')                    
