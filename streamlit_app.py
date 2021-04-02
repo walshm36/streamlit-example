43689cbc8523ce711b95ab615db42e67bdcaf289
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 16:18:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ def load_data(nrows):
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+data = load_data(10)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
