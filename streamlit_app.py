628df2bab241ff0d222d6e3a4481922c1400d964
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 16:15:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,7 @@ import streamlit as st
 
 
 st.title('Maeves Covid Dashboard')
-"""
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-"""
+
 DATA_URL = ('https://github.com/walshm36/streamlit-example/blob/762bfb00f06a5561781344d1136efd0edb8a221f/melted_cases.csv')
 
 def load_data(nrows):
