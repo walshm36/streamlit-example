73c46da56a7b727de1c5030fc875362c169c78c8
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 19:17:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 
 st.title('Maeves Covid Dashboard')                    
@@ -16,3 +17,7 @@ df = load_data()
 
 # show data on streamlit
 st.write(df)
+
+#graph
+fig = px.area(df, x="Date", y="1000 Cases", color="Country/Region")
+fig.show()
