ae8286b54a060103a0955f4cdba16a8d68a6caf1
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 16:01:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 
-st.title('My title')
+st.title('Maeves Covid Dashboard')
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
