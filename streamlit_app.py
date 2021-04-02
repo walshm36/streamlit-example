ea8daee6531431b181f42a8180c70ac2ddb6e05d
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-02 16:40:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,9 @@ import streamlit as st
 
 
 st.title('Maeves Covid Dashboard')
-
-
-"""
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv("Desktop/Project/melted.csv, nrows=nrows)
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
-
-st.subheader('Raw data')
-st.write(data)
-"""
                        
 
-df = pd.read_csv(".Desktop/Project/melted.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
+df = pd.read_csv(".Desktop/Project/melted.csv")  
 
 st.title("Hello world!")  # add a title
 st.write(df)
