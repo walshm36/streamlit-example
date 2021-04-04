d5a13a758c5b2e648ee1bbe509af15a25515aa8a
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-04 15:43:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import plotly as py
 import plotly.express as px
 import plotly.io as pio
 
-st.title('Maeves Covid Dashboard')                    
+st.title('Maeves Covid Dashboard :sunglasses:')                    
 
 DATA_URL = ('melted_cases.csv')
 @st.cache
@@ -16,12 +16,15 @@ def load_data():
     return data
 df = load_data()
 
+st.subheader('Raw data')
 st.write(df)
 
 fig = px.area(df, x="Date", y="1000 Cases", 
               color_discrete_sequence=px.colors.qualitative.Dark24,
               color="Country/Region",
               title ="Covid-19 Cases",
-              template="plotly_white")
+             # template="plotly_white"
+             )
 
+st.subheader('Graph: First Iteration')
 st.write(fig)
