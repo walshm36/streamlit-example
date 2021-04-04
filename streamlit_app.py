b632a6e1a66301f12b63fa4f0c21175814ac6861
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-04 18:48:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ df = load_data()
 st.subheader('Raw data')
 st.write(df)
 
+st.header("Cases Growth) 
+
 fig = px.area(df, x="Date", y="1000 Cases", 
               color_discrete_sequence=px.colors.qualitative.Dark24,
               color="Country/Region",
@@ -43,3 +45,10 @@ st.altair_chart(c, use_container_width=True)
 
 st.subheader('Graph: Third Iteration')
 
+values = st.sidebar.slider(“Date”, float(df.Date.min()), 1000., (50., 300.))
+f = px.area(df.query(f”Date.between{values}”), x="Date", y="1000 Cases", 
+              color_discrete_sequence=px.colors.qualitative.Dark24,
+              color="Country/Region",
+              title ="Covid-19 Cases"
+             )
+st.plotly_chart(f)
