9dd653a8a0de7cae38db3070ef32695e487e3f85
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-04-05 16:47:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.header("Cases Growth")
 
 fig = px.area(df, x="Date", y="1000 Cases", 
               color_discrete_sequence=px.colors.qualitative.Dark24,
-              color="Country_Region",
+              color="Continent",
               title ="Covid-19 Cases"
              )
 
